43b3c0123b1c22eb0c623fae5c213aca77c31f39
Tugrul Ates <me@tugrulates.com>
2024-11-20 16:53:35 +0100

    clean zsh config

--- init.zsh
+++ init.zsh
@@ -3,5 +3,7 @@
 dir=$(dirname $(readlink -f $0))
 
 # ~/
-unlink ~/.zshrc 2>/dev/null
-ln -s ${dir}/.zshrc ~/.zshrc
+for file in .zshrc .gitconfig .gitignore; do
+  unlink ~/${file} 2>/dev/null
+  ln -s ${dir}/${file} ~/${file}
+done
