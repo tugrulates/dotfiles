a3944b2c8e68516332a20bf935e09ba73974acb3
Tugrul Ates <me@tugrulates.com>
2025-11-03 00:49:48 +0100

    Don't link .git

--- init.bash
+++ init.bash
@@ -3,7 +3,7 @@
 dir=$(dirname $(readlink -f $0))
 
 # ~/
-for file in .bash* .inputrc .git* .config/*; do
+for file in .bashrc .bash_* .inputrc .git?* .config/*; do
 	unlink ~/${file} 2>/dev/null
 	mkdir -p $(dirname ~/${file})
 	ln -s ${dir}/${file} ~/${file}
--- init.zsh
+++ init.zsh
@@ -3,7 +3,7 @@
 dir=$(dirname $(readlink -f $0))
 
 # ~/
-for file in .zshrc .git* .config/*; do
+for file in .zshrc .git?* .config/*; do
 	unlink ~/${file} 2>/dev/null
 	mkdir -p $(dirname ~/${file})
 	ln -s ${dir}/${file} ~/${file}
