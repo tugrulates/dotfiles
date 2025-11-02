b03e67ea8a12d831b9d77beb300f2daca3a4136e
Tugrul Ates <me@tugrulates.com>
2025-11-03 00:40:44 +0100

    Simplify init scripts

--- init.bash
+++ init.bash
@@ -3,7 +3,7 @@
 dir=$(dirname $(readlink -f $0))
 
 # ~/
-for file in .bash_aliases .bash_profile .bashrc .gitattributes .gitconfig .gitignore .inputrc .config/starship.toml; do
+for file in .bash* .inputrc .git* .config/*; do
 	unlink ~/${file} 2>/dev/null
 	mkdir -p $(dirname ~/${file})
 	ln -s ${dir}/${file} ~/${file}
